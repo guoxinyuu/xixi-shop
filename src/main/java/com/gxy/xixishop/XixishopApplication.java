e0d86a25493a084c67cf833104ab699ae4933b7c
guoxinyuuuuuuu <1302241903@qq.com>
2024-02-03 13:55:34 +0800

    first

---
+++ src/main/java/com/gxy/xixishop/XixishopApplication.java
@@ -0,0 +1,13 @@
+package com.gxy.xixishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XixishopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XixishopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxy/xixishop/XixishopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxy.xixishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XixishopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
